676e3e5fb771650de76a5e2172d96d8131eb885b
chamjf23 <63682727+chamjf23@users.noreply.github.com>
2020-10-14 19:42:33 -0500

    Update tableaux.py

--- tableaux.py
+++ tableaux.py
@@ -4,6 +4,8 @@ from random import choice
 # Variables globales
 ##############################################################################
 
+# Crea los conectivos
+conectivos = ['Y', 'O', '>', '=']
 # Crea las letras minúsculas a-z
 letrasProposicionales = [chr(x) for x in range(97, 123)]
 # inicializa la lista de interpretaciones
@@ -32,16 +34,16 @@ def Inorder(f):
 	else:
 		return "(" + Inorder(f.left) + f.label + Inorder(f.right) + ")"
 
-def StringtoTree(A):
-    # Crea una formula como tree dada una formula como cadena escrita en notacion polaca inversa
-    # Input: A, lista de caracteres con una formula escrita en notacion polaca inversa
-             # letrasProposicionales, lista de letras proposicionales
-    # Output: formula como tree
+def String2Tree(A):
+	# Crea una formula como tree dada una formula como cadena escrita en notacion polaca inversa
+	# Input: - A, lista de caracteres con una formula escrita en notacion polaca inversa
+	#        - letrasProposicionales, lista de letras proposicionales
+	#        - conectivos, lista de conectivos
+	# Output: formula como tree
 
 	# OJO: DEBE INCLUIR SU CÓDIGO DE STRING2TREE EN ESTA PARTE!!!!!
 
-	p = letrasProposicionales[0] # ELIMINE ESTA LINEA LUEGO DE INCLUIR EL CODIGO DE STRING2TREE
-	return Tree(p, None, None) # ELIMINE ESTA LINEA LUEGO DE INCLUIR EL CODIGO DE STRING2TREE
+	pass
 
 ##############################################################################
 # Definición de funciones de tableaux
@@ -58,32 +60,78 @@ def imprime_hoja(H):
 		cadena += Inorder(f)
 	return cadena + "}"
 
+def complemento(l):
+	# Esta función devuelve el complemento de un literal
+	# Input: l, un literal
+	# Output: x, un literal
+
+	pass
+
 def par_complementario(l):
-	# Esta función determina si una lista de solo literales
-	# contiene un par complementario
-	# Input: l, una lista de literales
-	# Output: True/False
-	return False
+	for i in l:
+            for e in l:
+                if i == "-"+e:
+                    return True
+		return False
 
 def es_literal(f):
 	# Esta función determina si el árbol f es un literal
 	# Input: f, una fórmula como árbol
 	# Output: True/False
-	return False
+
+	pass
 
 def no_literales(l):
 	# Esta función determina si una lista de fórmulas contiene
 	# solo literales
 	# Input: l, una lista de fórmulas como árboles
 	# Output: None/f, tal que f no es literal
-	return False
 
-def clasifica_y_extiende(f):
-	# clasifica una fórmula como alfa o beta y extiende listaHojas
-	# de acuerdo a la regla respectiva
+	pass
+
+def clasificacion(f):
+    hijo1= tree.right
+    hijo2= tree.left
+    if tree.label == "Y":
+        return "alfa"
+    if tree.label == "-":
+        if hijo1.label == ("->") or hijo2.label ==("->"):
+        return "alfa"
+    if tree.label == "O":
+        return "beta"
+    if tree.label == "->":
+        return "beta"
+    if tree.label == "-":
+        if hijo1.label == ("Y") or hijo2.label ==("Y"):
+        return "beta"
+
+def clasifica_y_extiende(f, h):
+	# Extiende listaHojas de acuerdo a la regla respectiva
 	# Input: f, una fórmula como árbol
+	# 		 h, una hoja (lista de fórmulas como árboles)
 	# Output: no tiene output, pues modifica la variable global listaHojas
+
 	global listaHojas
+
+	print("Formula:", Inorder(f))
+	print("Hoja:", imprime_hoja(h))
+
+	assert(f in h), "La formula no esta en la lista!"
+
+	clase = clasificacion(f)
+	print("Clasificada como:", clase)
+	assert(clase != None), "Formula incorrecta " + imprime_hoja(h)
+
+	if clase == 'Alfa1':
+		aux = [x for x in h]
+		listaHojas.remove(h)
+		aux.remove(f)
+		aux += [f.right.right]
+		listaHojas.append(aux)
+	elif clase == 'Alfa2':
+		pass
+	# Aqui el resto de casos
+
 
 def Tableaux(f):
 
@@ -91,10 +139,13 @@ def Tableaux(f):
 	# Imput: - f, una fórmula como string en notación polaca inversa
 	# Output: interpretaciones: lista de listas de literales que hacen
 	#		 verdadera a f
+
 	global listaHojas
 	global listaInterpsVerdaderas
 
-	A = string2Tree(f)
+	A = String2Tree(f)
+	print(u'La fórmula introducida es:\n', Inorder(A))
+
 	listaHojas = [[A]]
 
-	return listaInterpsVerdaderas
+	pass
